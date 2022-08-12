6bd6bd2468d133a804a51136c2d6fc68b08ada81
ed-knowah <myemail>
2022-08-12 08:44:20 +0100

    :tada: Initial	commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'App';
+  options = ['angular', 'vue', 'react'];
+  skills: string[] = [];
+
+  onSubmit(value: any) {
+    value.skills = this.skills;
+    console.log(value);
+  }
+
+  addSkill(skill: string) {
+    this.skills.push(skill);
+    //console.log(this.skills);
+  }
 }
+  
